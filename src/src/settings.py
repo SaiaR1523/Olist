0237714633eac09268ce0cfc55325b4e798b303a
Saia Ralandison <112077063+SaiaR1523@users.noreply.github.com>
2023-05-02 14:16:43 +0400

    Update settings.py

--- src/src/settings.py
+++ src/src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ivwejitpoip7h^)l)k3ghhogsigi&b-xa58yo2_6$h_qn7=25)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
